7cc15faa1b66b8a198731fea5c88cb6319b3afa8
Prajwal <87972700+Prajwal433@users.noreply.github.com>
2023-03-12 15:52:37 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -26,42 +26,55 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <div class= "about-tech" >Javascript</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <div class= "about-tech" >Nodejs</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <div class= "about-tech" >React</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiExpress />
+        <div class= "about-tech" >Express</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGnubash />
+        <div class= "about-tech" >Bash</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
+        <div class= "about-tech" >NextJS</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <AiFillHtml5 />
+        <div class= "about-tech" >HTML</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPhp />
+        <div class= "about-tech" >PHP</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiCss3 />
+        <div class= "about-tech" >Css</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJson />
+        <div class= "about-tech" >Json</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+        <div class= "about-tech" >MongoDB</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMysql />
+        <div class= "about-tech" >MySQL</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPostgresql />
+        <div class= "about-tech" >PostgreSQL</div>
       </Col>
     </Row>
   );
