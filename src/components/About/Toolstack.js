1e314a73708237d95da4e73154e7c4770be2803a
Prajwal <87972700+Prajwal433@users.noreply.github.com>
2023-03-12 15:54:10 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -14,18 +14,23 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
+        <div class= "about-tech" >Linux</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiUbuntu />
+        <div class= "about-tech" >Ubuntu</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <div class= "about-tech" >Vsc</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGit />
+        <div class= "about-tech" >Git</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
+        <div class= "about-tech" >Github</div>
       </Col>
     </Row>
   );
